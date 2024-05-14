1fbfebd8d20e351cb0f6fb3567eaef7925755a87
AJIeksand <149382802+AJIeksand@users.noreply.github.com>
2024-05-14 15:18:12 +0300

    Task1

---
+++ Task1/src/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+
+    private static void printThreeWords() {
+        System.out.println("Orange\nBanana\nApple");
+    }
+
+
+}
