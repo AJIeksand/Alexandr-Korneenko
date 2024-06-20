36e6a66172cba7d63fade73e79efe48aefe210ca
AJIeksand <149382802+AJIeksand@users.noreply.github.com>
2024-06-20 09:50:28 +0300

    Add files via upload
    Lesson_12_Corrected

---
+++ src/ArrayConverter.java
@@ -0,0 +1,38 @@
+public class ArrayConverter {
+    public static void main(String[] args) {
+        String[][] array = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
+
+        try {
+            checkArraySize(array);
+            int sum = sumArray(array);
+            System.out.println("Сумма элементов массива: " + sum);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void checkArraySize(String[][] array) throws MyArraySizeException {
+        if (array.length != 4 || array[0].length != 4) {
+            throw new MyArraySizeException("Массив другого размера. Должен быть 4x4");
+        }
+    }
+
+    private static int sumArray(String[][] array) throws MyArrayDataException {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Неверные данные в ячейке [" + i + "][" + j + "]");
+                }
+            }
+        }
+        return sum;
+    }
+}
